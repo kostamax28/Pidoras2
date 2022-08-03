65acd408e4819b1e108df062546fdca3f6c5655f
kostamax28 <96413785+kostamax28@users.noreply.github.com>
2022-08-03 19:10:05 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+token = '5389202921:AAG3u6q5gkmvacDKH31vNnkOaUXgrVaaeG4' # Токен бота
+
+loginbot = 'MegaOsinterbot' # Логин бота телеграм
+
+QIWI_PUBLICKEY = 'xxxxxx' #p2p public key
+
+QIWI_TOKEN = 'xxxxxxxx' # токен киви
+
+QIWI_NUMBER = '79633754351' # Номер киви без +
+
+admin_id = 5403539045 # Ваш айди
+
+price = 190 # Цена за доступ
+
+#Ссылка на канал - @cphyof
+
+#Наш чат - @cphyofchat
